cb67aef716d66c47691f129f47237a3bf508377e
KimSuhong <ksum0310@naver.com>
2022-12-26 18:52:07 +0900

    프로젝트 최초 커밋

---
+++ pybo/models.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
+
+
+# class Category(models.Model):
+#     name = models.CharField(max_length=20, unique=True)
+#     description = models.CharField(max_length=200, null=True, blank=True)
+#     has_answer = models.BooleanField(default=True)  # 답변가능여부
+#
+#     def __str__(self):
+#         return self.name
+#
+#     def get_absolute_url(self):
+#         return reverse('pybo:index', args=[self.name])
+
+
+class Question(models.Model):
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category_question')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')  # 다대다 관계
+
+    def __str__(self):
+        return self.subject
+
+
+class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
